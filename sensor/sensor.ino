d0b89d5886fad21c2798af57cea21fab6f0628bb
Adit <aditdesai2002@gmail.com>
2022-04-07 02:31:01 +0530

    Updated arduino code and ui

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,12 +1,14 @@
 #include "DHT.h"
 
 const int sensor = A0;
+const int motorPin = 3;
 DHT dht(sensor, DHT11);
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(sensor, INPUT);
+  pinMode(motorPin, OUTPUT);
   dht.begin();
 }
 
@@ -15,4 +17,17 @@ void loop()
   delay(1000);
   float t = dht.readTemperature();
   Serial.println(t);
+
+  if (t < 20)
+  {
+    analogWrite(motorPin, 50);
+  }
+  else if (t < 30)
+  {
+    analogWrite(motorPin, 150);
+  }
+  else
+  {
+    analogWrite(motorPin, 255);
+  }
 }
